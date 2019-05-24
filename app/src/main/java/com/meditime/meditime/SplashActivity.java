b0c97e1f7794daa878d2ad1bb4db10f4ed97b530
Jeet <jeetthakare@hotmail.com>
2019-05-23 18:04:32 -0700

    Added splash screen

---
+++ app/src/main/java/com/meditime/meditime/SplashActivity.java
@@ -0,0 +1,36 @@
+package com.meditime.meditime;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private Button login;
+    private Button signUp;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        login = findViewById(R.id.login);
+        signUp = findViewById(R.id.signup);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent= new Intent(SplashActivity.this,LoginActivity.class);
+                startActivity(intent);
+            }});
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(SplashActivity.this, "Hello", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
