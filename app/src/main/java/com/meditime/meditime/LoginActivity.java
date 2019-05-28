b81ce90da830cecb62b77570e89dc7e17056236c
mehta96aayushi <aayushi.mehta596@gmail.com>
2019-05-27 21:37:59 -0700

    Signup layout created. Signup activity file updated. Login activity and login layout updated. minSdk version has been changed from 28 to 27 in build.gradle file.

--- app/src/main/java/com/meditime/meditime/LoginActivity.java
+++ app/src/main/java/com/meditime/meditime/LoginActivity.java
@@ -7,7 +7,6 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -32,7 +31,7 @@ public class LoginActivity extends AppCompatActivity {
         username = findViewById(R.id.usernameTV);
         pass = findViewById(R.id.passwordTV);
 
-        loginBtn = findViewById(R.id.loginBtn);
+        loginBtn = findViewById(R.id.loginButton);
 
         mAuth = FirebaseAuth.getInstance();
 
