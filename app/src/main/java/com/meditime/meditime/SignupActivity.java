bdc665a34fe7ca88c6f8909ce3f8685b749e28b7
mehta96aayushi <aayushi.mehta596@gmail.com>
2019-05-30 16:03:18 -0700

    adding spinners and backend logic

--- app/src/main/java/com/meditime/meditime/SignupActivity.java
+++ app/src/main/java/com/meditime/meditime/SignupActivity.java
@@ -1,8 +1,6 @@
 package com.meditime.meditime;
 
 import android.content.Intent;
-import android.graphics.Color;
-import android.graphics.PorterDuff;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -17,10 +15,21 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 
 public class SignupActivity extends AppCompatActivity {
@@ -30,25 +39,47 @@ public class SignupActivity extends AppCompatActivity {
     Spinner genderSpinner, roleSpinner, doctorSpinner;
     EditText name, age, email, password, confirmPwd;
     private FirebaseAuth mAuth;
+    private DocumentReference mDocRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
 
-        name=findViewById(R.id.nameTxt);
-        age=findViewById(R.id.ageTxt);
-        email=findViewById(R.id.emailTxt);
-        password=findViewById(R.id.passwordTxt);
-        confirmPwd=findViewById(R.id.confirmPwdText);
+        name = findViewById(R.id.nameTxt);
+        age = findViewById(R.id.ageTxt);
+        email = findViewById(R.id.emailTxt);
+        password = findViewById(R.id.passwordTxt);
+        confirmPwd = findViewById(R.id.confirmPwdText);
 
-        genderSpinner=findViewById(R.id.genderSpinner);
-        roleSpinner=findViewById(R.id.spinner2);
-        doctorSpinner=findViewById(R.id.doctorSpinner);
+        genderSpinner = findViewById(R.id.genderSpinner);
+        roleSpinner = findViewById(R.id.spinner2);
+        doctorSpinner = findViewById(R.id.doctorSpinner);
+        doctorLbl = findViewById(R.id.doctorLabel);
 
-        doctorLbl=findViewById(R.id.doctorLabel);
+        final ArrayAdapter<User> docsAdapter = new ArrayAdapter<>(this, R.layout.support_simple_spinner_dropdown_item, getDoctors());
+        doctorSpinner.setAdapter(docsAdapter);
 
-        signupButton=(Button)findViewById(R.id.signupButton);
+//        String compareValue="doctor";
+//        if (compareValue != null) {
+//            int spinnerPosition = docsAdapter.getPosition(compareValue);
+//            doctorSpinner.setSelection(spinnerPosition);
+//        }
+
+
+        doctorSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        signupButton = (Button) findViewById(R.id.signupButton);
 
         ArrayAdapter<CharSequence> genderAdapter = ArrayAdapter.createFromResource(this, R.array.gender_dropdown, android.R.layout.simple_spinner_item);
         genderAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
@@ -57,16 +88,16 @@ public class SignupActivity extends AppCompatActivity {
         ArrayAdapter<CharSequence> roleAdapter = ArrayAdapter.createFromResource(this, R.array.role_dropdown, android.R.layout.simple_spinner_item);
         roleAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         roleSpinner.setAdapter(roleAdapter);
-        doctorLbl=findViewById(R.id.doctorLabel);
 
-        final int currentChoice=roleSpinner.getSelectedItemPosition();
+        doctorLbl = findViewById(R.id.doctorLabel);
 
+        final int currentChoice = roleSpinner.getSelectedItemPosition();
         roleSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            public void onItemSelected(AdapterView<?> adapterView,View view,int i,long id) {
-                if(currentChoice==i){
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long id) {
+                if (currentChoice == i) {
                     doctorLbl.setVisibility(View.VISIBLE);
                     doctorSpinner.setVisibility(View.VISIBLE);
-                }else{
+                } else {
                     doctorLbl.setVisibility(View.GONE);
                     doctorSpinner.setVisibility(View.GONE);
                 }
@@ -81,48 +112,132 @@ public class SignupActivity extends AppCompatActivity {
         signupButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                if (!validateform()) {
+                    Toast.makeText(SignupActivity.this, "Error in the input.",
+                            Toast.LENGTH_SHORT).show();
+                    return;
+                }
                 mAuth.createUserWithEmailAndPassword(email.getText().toString(), password.getText().toString())
                         .addOnCompleteListener(SignupActivity.this, new OnCompleteListener<AuthResult>() {
                             @Override
                             public void onComplete(@NonNull Task<AuthResult> task) {
                                 if (task.isSuccessful()) {
+
                                     // Sign in success, update UI with the signed-in user's information
                                     Log.d("Sign Up", "createUserWithEmail:success");
-                                    FirebaseUser user = mAuth.getCurrentUser();
-                                    //updateUI(user);
+                                    final FirebaseUser user = mAuth.getCurrentUser();
+                                    if (user == null) {
+                                        Toast.makeText(SignupActivity.this, "Authentication details failed.",
+                                                Toast.LENGTH_SHORT).show();
+                                        user.delete();
+                                        return;
+                                    }
+                                    mDocRef = FirebaseFirestore.getInstance().document("users/" + user.getEmail());
+                                    HashMap<String, String> userDetails = new HashMap<>();
+                                    userDetails.put("Name", name.getText().toString());
+                                    userDetails.put("Age", age.getText().toString());
+                                    userDetails.put("Gender", genderSpinner.getSelectedItem().toString());
+                                    userDetails.put("Role", roleSpinner.getSelectedItem().toString());
+                                    if (roleSpinner.getSelectedItem().toString().equals("Patient")) {
+                                        userDetails.put("Doctor", doctorSpinner.getSelectedItem().toString());
+                                    }
+
+                                    mDocRef.set(userDetails).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                        @Override
+                                        public void onSuccess(Void aVoid) {
+                                            Toast.makeText(SignupActivity.this, "You have successfully signed up!",
+                                                    Toast.LENGTH_SHORT).show();
+
+                                            Intent intent = new Intent(SignupActivity.this, LoginActivity.class);
+                                            startActivity(intent);
+                                            finish();
+                                        }
+                                    }).addOnFailureListener(new OnFailureListener() {
+                                        @Override
+                                        public void onFailure(@NonNull Exception e) {
+                                            Toast.makeText(SignupActivity.this, "Authentication failed.",
+                                                    Toast.LENGTH_SHORT).show();
+                                            user.delete();
+                                        }
+                                    });
+
+
                                 } else {
                                     // If sign in fails, display a message to the user.
                                     Log.w("Sign Up", "createUserWithEmail:failure", task.getException());
                                     Toast.makeText(SignupActivity.this, "Authentication failed.",
                                             Toast.LENGTH_SHORT).show();
-                                    //updateUI(null);
                                 }
-
-                                // ...
                             }
                         });
-//                if( name.getText().toString().isEmpty()){
-//                    name.setError("Name is required!");
-//                }if( age.getText().toString().isEmpty()){
-//                    age.setError("Age is required!");
-//                }if( gender.getText().toString().isEmpty()){
-//                    gender.setError("Gender is required!");
-//                }if( email.getText().toString().isEmpty()){
-//                    email.setError("Email is required!");
-//                }if( password.getText().toString().isEmpty()){
-//                    password.setError("Password is required!");
-//                }if( confirmPwd.getText().toString().isEmpty()){
-//                    confirmPwd.setError("This is required!");
-//                }if( role.getText().toString().isEmpty()){
-//                    role.setError("Role is required!");
-//                }else{
-//                    Toast.makeText(SignupActivity.this, "You have successfully signed up!",
-//                            Toast.LENGTH_SHORT).show();
-//
-//                    Intent intent= new Intent(SignupActivity.this,LoginActivity.class);
-//                    startActivity(intent);
-//                }
             }
         });
     }
+
+    private boolean validateform() {
+        if (name.getText().toString().isEmpty()) {
+            name.setError("Name is required!");
+            return false;
+        }
+        if (age.getText().toString().isEmpty()) {
+            age.setError("Age is required!");
+            return false;
+        }
+        if (email.getText().toString().isEmpty()) {
+            email.setError("Email is required!");
+            return false;
+        } else {
+            String regExpn =
+                    "^(([\\w-]+\\.)+[\\w-]+|([a-zA-Z]{1}|[\\w-]{2,}))@"
+                            + "((([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])\\.([0-1]?"
+                            + "[0-9]{1,2}|25[0-5]|2[0-4][0-9])\\."
+                            + "([0-1]?[0-9]{1,2}|25[0-5]|2[0-4][0-9])\\.([0-1]?"
+                            + "[0-9]{1,2}|25[0-5]|2[0-4][0-9])){1}|"
+                            + "([a-zA-Z]+[\\w-]+\\.)+[a-zA-Z]{2,4})$";
+
+            Pattern pattern = Pattern.compile(regExpn, Pattern.CASE_INSENSITIVE);
+            Matcher matcher = pattern.matcher(email.getText().toString());
+
+            if (!matcher.matches()) {
+                email.setError("Enter valid email!");
+                return false;
+            }
+        }
+        if (password.getText().toString().isEmpty()) {
+            password.setError("Password is required!");
+            return false;
+        }
+        if (confirmPwd.getText().toString().isEmpty()) {
+            confirmPwd.setError("This is required!");
+            return false;
+        }
+        if (!password.getText().toString().equals(confirmPwd.getText().toString())) {
+            confirmPwd.setError("Password mismatch!");
+            return false;
+        }
+        return true;
+    }
+
+    private ArrayList<User> getDoctors() {
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        final ArrayList<User> docs = new ArrayList<>();
+        db.collection("users")
+                .whereEqualTo("Role", "Doctor")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                docs.add(new User(document.getId(), document.getString("Name"), document.getString("Role"), document.getString("Gender")));
+                                //Log.d("DB", document.getId() + " => " + document.getData());
+                            }
+                        } else {
+                            Log.d("DB", "Error getting documents: ", task.getException());
+                        }
+                    }
+                });
+        return docs;
+    }
+
 }
