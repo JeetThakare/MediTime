599e4457ba7a13eec14b4089686956e138e833b2
Niranjan <niranjanakolkar@gmail.com>
2019-06-02 15:36:12 -0700

    added validation for empty username password on login activity, add date picker in medicine details and its validations

--- app/src/main/java/com/meditime/meditime/LoginActivity.java
+++ app/src/main/java/com/meditime/meditime/LoginActivity.java
@@ -44,62 +44,79 @@ public class LoginActivity extends AppCompatActivity {
         loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                mAuth.signInWithEmailAndPassword(username.getText().toString(), pass.getText().toString())
-                        .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
-                            @Override
-                            public void onComplete(@NonNull Task<AuthResult> task) {
-                                if (task.isSuccessful()) {
-                                    // Sign in success, update UI with the signed-in user's information
-                                    Log.d("LogIn", "signInWithEmail:success");
-                                    System.out.println("signInWithEmail:success");
-                                    FirebaseUser user = mAuth.getCurrentUser();
-                                    Toast.makeText(LoginActivity.this, user.getEmail()+"logged in successfully", Toast.LENGTH_SHORT).show();
+                if(isValidate()){
+                    mAuth.signInWithEmailAndPassword(username.getText().toString(), pass.getText().toString())
+                            .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (task.isSuccessful()) {
+                                        // Sign in success, update UI with the signed-in user's information
+                                        Log.d("LogIn", "signInWithEmail:success");
+                                        System.out.println("signInWithEmail:success");
+                                        FirebaseUser user = mAuth.getCurrentUser();
+                                        Toast.makeText(LoginActivity.this, user.getEmail()+"logged in successfully", Toast.LENGTH_SHORT).show();
 
-                                    FirebaseFirestore db = FirebaseFirestore.getInstance();
-                                    DocumentReference docRef = db.collection("users").document(user.getEmail()); // user is current user here
-                                    docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
-                                        @Override
-                                        public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-                                            if (task.isSuccessful()) {
-                                                DocumentSnapshot document = task.getResult();
-                                                if (document.exists()) {
+                                        FirebaseFirestore db = FirebaseFirestore.getInstance();
+                                        DocumentReference docRef = db.collection("users").document(user.getEmail()); // user is current user here
+                                        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                                            @Override
+                                            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                                                if (task.isSuccessful()) {
+                                                    DocumentSnapshot document = task.getResult();
+                                                    if (document.exists()) {
 //                                                    Log.d(TAG, "DocumentSnapshot data: " + document.getString("Role"));
-                                                    role = document.getString("Role");
-                                                    System.out.println("role:"+ role);
-                                                    if(role.equals("Doctor")){
-                                                        startActivity(new Intent(LoginActivity.this, DrHome.class));
-                                                    }
+                                                        role = document.getString("Role");
+                                                        System.out.println("role:"+ role);
+                                                        if(role.equals("Doctor")){
+                                                            startActivity(new Intent(LoginActivity.this, DrHome.class));
+                                                        }
 //                                                    else{
 //                                                        startActivity(new Intent(LoginActivity.this, PatientHome.class))
 //                                                    }
+                                                    } else {
+                                                        Log.d("Login role check", "No such document");
+                                                    }
                                                 } else {
-                                                    Log.d("Login role check", "No such document");
+                                                    Log.d("Login role check", "get failed with ", task.getException());
                                                 }
-                                            } else {
-                                                Log.d("Login role check", "get failed with ", task.getException());
+
                                             }
-
-                                        }
-                                    });
+                                        });
 
 
-                                    // Open relevant activity
-                                    // Code here ...
-                                    // startActivity(new Intent(LoginActivity.this, SplashActivity.class));
+                                        // Open relevant activity
+                                        // Code here ...
+                                        // startActivity(new Intent(LoginActivity.this, SplashActivity.class));
 
-                                    // Open relevant activity
-                                    finish();
-                                } else {
-                                    // If sign in fails, display a message to the user.
-                                    Log.w("LogIn", "signInWithEmail:failure", task.getException());
-                                    Toast.makeText(LoginActivity.this, "Authentication failed." + task.getException().toString(),
-                                            Toast.LENGTH_SHORT).show();
+                                        // Open relevant activity
+                                        finish();
+                                    } else {
+                                        // If sign in fails, display a message to the user.
+                                        Log.w("LogIn", "signInWithEmail:failure", task.getException());
+                                        Toast.makeText(LoginActivity.this, "Authentication failed." + task.getException().toString(),
+                                                Toast.LENGTH_SHORT).show();
 
+                                    }
                                 }
-                            }
-                        });
+                            });
+                }
+
             }
         });
 
+
+
+    }
+
+    private boolean isValidate(){
+        if(username.getText().toString().isEmpty()){
+            username.setError("Username is required");
+            return false;
+        }
+        if (pass.getText().toString().isEmpty() ){
+            pass.setError("Password is required");
+            return false;
+        }
+        return true;
     }
 }
--- app/src/main/java/com/meditime/meditime/medicineDetails.java
+++ app/src/main/java/com/meditime/meditime/medicineDetails.java
@@ -1,17 +1,25 @@
 package com.meditime.meditime;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.icu.util.Calendar;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class medicineDetails extends AppCompatActivity {
 
@@ -19,6 +27,9 @@ public class medicineDetails extends AppCompatActivity {
     ImageView medicineImage;
     EditText name, schedule, frequency, startDate, endDate;
     private FirebaseAuth auth;
+    private Calendar calendar1;
+    private Calendar calendar2;
+    private DatePickerDialog datePickerDialog;
 
     private static final int REQUEST_IMAGE_CAPTURE = 101;
 
@@ -28,19 +39,23 @@ public class medicineDetails extends AppCompatActivity {
         setContentView(R.layout.activity_medicine_details);
         setTitle("Medicine Details");
         photoBtn = (Button) findViewById(R.id.photoBtn);
-        saveBtn=findViewById(R.id.saveBtn);
+        saveBtn = findViewById(R.id.saveBtn);
         medicineImage = (ImageView) findViewById(R.id.medicineIV);
 
-        name=findViewById(R.id.nameET);
-        schedule=findViewById(R.id.scheduleET);
-        frequency=findViewById(R.id.frequencyET);
-        startDate=findViewById(R.id.startDateET);
-        endDate=findViewById(R.id.endDateET);
+        name = findViewById(R.id.nameET);
+        schedule = findViewById(R.id.scheduleET);
+        frequency = findViewById(R.id.frequencyET);
+        startDate = findViewById(R.id.startDateET);
+        startDate.setInputType(InputType.TYPE_NULL);
+        calendar1 = Calendar.getInstance();
+        calendar2 = Calendar.getInstance();
+        endDate = findViewById(R.id.endDateET);
+        endDate.setInputType(InputType.TYPE_NULL);
 
         auth = FirebaseAuth.getInstance();
         FirebaseUser user = auth.getCurrentUser();
-        Intent intent=getIntent();
-        if (intent.getStringExtra("email")==null){
+        Intent intent = getIntent();
+        if (intent.getStringExtra("email") == null) {
             name.setText(intent.getStringExtra("name"));
             schedule.setText(intent.getStringExtra("schedule"));
             frequency.setText(intent.getStringExtra("weekfreq"));
@@ -48,7 +63,7 @@ public class medicineDetails extends AppCompatActivity {
             endDate.setText(intent.getStringExtra("enddt"));
 //            set medicineImage url
         }
-        if(intent.getStringExtra("role").matches("Patient")){
+        if (intent.getStringExtra("role").matches("Patient")) {
             name.setFocusable(false);
             schedule.setFocusable(false);
             frequency.setFocusable(false);
@@ -67,40 +82,106 @@ public class medicineDetails extends AppCompatActivity {
         saveBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                if (validate()) {
+                    System.out.println("Data Validated");
+                }
 
+            }
+        });
+
+        startDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                calendar=Calendar.getInstance();
+                int day = calendar1.get(Calendar.DAY_OF_MONTH);
+                int month = calendar1.get(Calendar.MONTH);
+                int year = calendar1.get(Calendar.YEAR);
+
+                datePickerDialog = new DatePickerDialog(medicineDetails.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int myear, int mmonth, int mday) {
+                        startDate.setText(mmonth + 1 + "/" + mday + "/" + myear);
+                    }
+                }, month, day, year);
+                datePickerDialog.show();
+            }
+        });
+
+        endDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                calendar=Calendar.getInstance();
+                int day = calendar2.get(Calendar.DAY_OF_MONTH);
+                int month = calendar2.get(Calendar.MONTH);
+                int year = calendar2.get(Calendar.YEAR);
+
+
+                datePickerDialog = new DatePickerDialog(medicineDetails.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int myear, int mmonth, int mday) {
+                        SimpleDateFormat sdf = new SimpleDateFormat("mm/dd/yyyy");
+                        endDate.setText(mmonth + 1 + "/" + mday + "/" + myear);
+
+                    }
+                }, month, day, year);
+                datePickerDialog.show();
             }
         });
     }
 
     @Override
-    protected void onActivityResult(int requestCode,int resultCode,Intent data){
-        if (requestCode==REQUEST_IMAGE_CAPTURE && resultCode==RESULT_OK){
-            Bundle extras=data.getExtras();
-            Bitmap bitmap= (Bitmap)extras.get("data");
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap bitmap = (Bitmap) extras.get("data");
             medicineImage.setImageBitmap(bitmap);
         }
 
     }
 
-    private boolean validate(){
-            if (name.getText().toString().isEmpty()) {
-                name.setError("Name is required!");
-                return false;
-            }
-            if (schedule.getText().toString().isEmpty()) {
-                schedule.setError("Schedule is required!");
-                return false;
-            }
-            if (frequency.getText().toString().isEmpty()) {
-                frequency.setError("Weekly frequency is required!");
-                return false;
-            }
-            if (startDate.getText().toString().isEmpty()) {
-                name.setError("Name is required!");
-                return false;
-            }
+    private boolean validate() {
+        if (name.getText().toString().isEmpty()) {
+            name.setError("Name is required!");
+            return false;
+        }
+        if (schedule.getText().toString().isEmpty()) {
+            schedule.setError("Schedule is required!");
+            return false;
+        }
+        if (frequency.getText().toString().isEmpty()) {
+            frequency.setError("Weekly frequency is required!");
+            return false;
+        }
+        if (startDate.getText().toString().isEmpty()) {
+            startDate.setError("Start date is required!");
+            return false;
+        }
+        if (endDate.getText().toString().isEmpty()) {
+            endDate.setError("End date is required!");
+            return false;
+        }
+        if (!(validateEndDate())) {
+            endDate.setError("End date should be after start date");
+            return false;
+        }
+//
 
-            return true;
+        return true;
+
+    }
+
+    private boolean validateEndDate() {
+        try {
+            Date strdate = new SimpleDateFormat("mm/dd/yyyy").parse(startDate.getText().toString());
+            Date enddate = new SimpleDateFormat("mm/dd/yyyy").parse(endDate.getText().toString());
+            if (strdate.before(enddate)) {
+                return true;
+            }
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return false;
+
 
     }
 }
